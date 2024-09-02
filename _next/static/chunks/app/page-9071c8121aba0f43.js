9724518162685e0924b3f52a86ed13771f5cc621
buapebot <119761697+buapebot@users.noreply.github.com>
2024-09-02 01:17:49 +0000

    deploy: f787d9724591924fd6299239fc69a115974f01bb

---
+++ _next/static/chunks/app/page-9071c8121aba0f43.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{5730:function(){}},function(n){n.O(0,[910,500,744],function(){return n(n.s=5730)}),_N_E=n.O()}]);
--- assets/js/6c2ac63f.06d8dcb2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[809],{782:e=>{e.exports=JSON.parse('{"banner":"","breadcrumbs":true,"gitRefName":"master","minimal":false,"pluginId":"default","scopes":[]}')}}]);
